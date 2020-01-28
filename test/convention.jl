754dfce7ad07edfe1074d028a83e7fc4321d156d
Thibaut Lienart <ltib@me.com>
2020-01-28 09:16:17 +0100

    warning test noconvention

--- test/convention.jl
+++ test/convention.jl
@@ -1,20 +1,24 @@
-struct MockMLJ <: Convention end
-set_convention(MockMLJ)
-
-TRAIT_FUNCTION_GIVEN_NAME[:table] = Tables.istable
-
-isjunk(::Any)     = false
-isjunk(s::String) = s == "junk" ? true : false
-TRAIT_FUNCTION_GIVEN_NAME[:junk] = isjunk
-
-ScientificTypes.info(object, ::Val{:junk}) = length(object)
-
 @testset "convention" begin
+    set_convention(ScientificTypes.NoConvention
+    c = ""
+    @test_logs (:warn, "No convention specified. Did you forget to use the `set_convention` function?") (c = convention())
+    @test c isa ScientificTypes.NoConvention
+
+    struct MockMLJ <: Convention end
+    set_convention(MockMLJ)
     c = convention()
     @test c isa MockMLJ
 end
 
 @testset "trait" begin
+    TRAIT_FUNCTION_GIVEN_NAME[:table] = Tables.istable
+
+    isjunk(::Any)     = false
+    isjunk(s::String) = s == "junk" ? true : false
+    TRAIT_FUNCTION_GIVEN_NAME[:junk] = isjunk
+
+    ScientificTypes.info(object, ::Val{:junk}) = length(object)
+
     X = (x = [1,2,3],
          y = [5,5,7])
     @test trait(X)     == :table
