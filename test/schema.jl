edef159c7227f4fd05709ef66fac359cdeb3b022
Thibaut Lienart <ltib@me.com>
2020-01-28 09:51:02 +0100

    last bit of coverage

--- test/schema.jl
+++ test/schema.jl
@@ -1,5 +1,6 @@
 @testset "Schema" begin
     sch = S.Schema((:a, :b), (Int, Int), (Count, Count), 5)
+    @test sch isa S.Schema{(:a, :b),Tuple{Int64,Int64},Tuple{Count,Count},5}
     @test sch.names == (:a, :b)
     @test sch.types == (Int, Int)
     @test sch.scitypes == (Count, Count)
