b37117dc504ab525b704eef889b3f284e67dba84
Thibaut Lienart <ltib@me.com>
2020-01-30 12:47:48 +0100

    adding  1.0 on CI

--- test/convention.jl
+++ test/convention.jl
@@ -6,7 +6,6 @@ struct MockMLJ <: Convention end
     @test_logs (:warn, "No convention specified. Did you forget to use the `set_convention` function?") (c = ST.convention())
     @test c isa ST.NoConvention
 
-    struct MockMLJ <: Convention end
     ST.set_convention(MockMLJ())
     c = ST.convention()
     @test c isa MockMLJ
