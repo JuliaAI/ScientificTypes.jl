f3be187a18721ddbe27437c494b425cc258d55b6
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-04-21 13:10:19 +1200

    revise temporal types as per #14 (breaking)

--- src/ScientificTypes.jl
+++ src/ScientificTypes.jl
@@ -6,7 +6,9 @@ export Convention
 # re-export-able types and methods
 export Scientific, Found, Unknown, Known, Finite, Infinite,
     OrderedFactor, Multiclass, Count, Continuous,
-    Time, Day, Instant, Textual, Binary,
+    ScientificTimeType, ScientificDate, ScientificDateTime,
+    ScientificTime,
+    Textual, Binary,
     ColorImage, GrayImage, Image, Table
 export scitype, scitype_union, elscitype, nonmissing, trait
 
@@ -20,12 +22,12 @@ abstract type Found          end
 abstract type Known <: Found end
 struct      Unknown <: Found end
 
-abstract type   Infinite <: Known end
-abstract type  Finite{N} <: Known end
-abstract type Image{W,H} <: Known end
-abstract type Time <: Known end
-struct           Textual <: Known end
-struct          Table{K} <: Known end
+abstract type           Infinite <: Known end
+abstract type          Finite{N} <: Known end
+abstract type         Image{W,H} <: Known end
+abstract type ScientificTimeType <: Known end
+struct                   Textual <: Known end
+struct                  Table{K} <: Known end
 
 struct Continuous <: Infinite end
 struct      Count <: Infinite end
@@ -33,11 +35,12 @@ struct      Count <: Infinite end
 struct    Multiclass{N} <: Finite{N} end
 struct OrderedFactor{N} <: Finite{N} end
 
-struct Day <: Time end
-struct Instant <: Time end
+struct     ScientificDate <: ScientificTimeType end
+struct     ScientificTime <: ScientificTimeType end
+struct ScientificDateTime <: ScientificTimeType end
 
-struct    GrayImage{W,H} <: Image{W,H} end
-struct   ColorImage{W,H} <: Image{W,H} end
+struct  GrayImage{W,H} <: Image{W,H} end
+struct ColorImage{W,H} <: Image{W,H} end
 
 # aliases:
 const Binary     = Finite{2}
