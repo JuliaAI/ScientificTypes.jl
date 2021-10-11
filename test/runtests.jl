cecd1eb3bc0c22c25e7777073ce1347cf20871ab
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-10-11 17:20:46 +1300

    again

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test, ScientificTypes, ScientificTypesBase, Random
 using Tables, CategoricalArrays, DataFrames
-using ColorTypes, CorpusLoaders
+using ColorTypes
 using Dates
 # using CSV # dropped until julia release new LTS as issue for 1.0
 import Distributions
