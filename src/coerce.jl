129fc0d3968f7350b634fd5d438229ff70718c4e
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-11-04 12:08:50 +1300

    typo doc-string

--- src/coerce.jl
+++ src/coerce.jl
@@ -171,10 +171,10 @@ end
 """
 coerce!(X, ...)
 
-Same as [`ScientificTypes.coerce`](@ref) except it does the modification in
-place provided `X` supports in-place modification (at the moment, only the
-DataFrame! does). An error is thrown otherwise. The arguments are the same as
-`coerce`.
+Same as [`ScientificTypes.coerce`](@ref) except it does the
+modification in place provided `X` supports in-place modification (eg,
+DataFrames). An error is thrown otherwise. The arguments are the same
+as `coerce`.
 
 """
 coerce!(X, a...;  kw...) = begin
