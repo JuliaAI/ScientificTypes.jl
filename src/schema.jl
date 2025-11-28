c0a44903a9174c54398442876147aa23b6cf3e26
Anthony D. Blaom <anthony.blaom@gmail.com>
2025-11-28 16:47:50 +1300

    get rid of bold headings in display of schema

--- src/schema.jl
+++ src/schema.jl
@@ -244,7 +244,11 @@ function _rows_schema(rows, sch::Tables.Schema{nothing, nothing})
 end
 
 function Base.show(io::IO, ::MIME"text/plain", s::Schema)
-#    data = Tables.matrix(s)
+    # for getting rid of bold in table headings:
+    style = PrettyTables.TextTableStyle(
+        first_line_column_label = PrettyTables.crayon"black",
+    )
+
     column_labels = [["names", "scitypes", "types"],]
-    pretty_table(io, s; column_labels, alignment=:l)
+    pretty_table(io, s; column_labels, style, alignment=:l)
 end
